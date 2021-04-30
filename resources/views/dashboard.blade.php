0e2a98e1ec473db7b6bb1289b07e2ae330905379
ServicesPro <77097307+ServicesPro@users.noreply.github.com>
2021-04-30 17:02:49 +0000

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -224,5 +224,74 @@
                 overlay.classList.add('hidden')
             })
         })
+        
+        const MINUTES = 60
+        const HOURS = 60 * MINUTES
+        const DAYS = 24 * HOURS
+
+        const elements = {
+            days: document.getElementById('#days'),
+            hours: document.getElementById('#hours'),
+            minutes: document.getElementById('#minutes'),
+            seconds: document.getElementById('#seconds')
+        }
+
+        const $days = document.getElementById('#days')
+        const $hours = document.getElementById('#hours')
+        const $minutes = document.getElementById('#minutes')
+        const $seconds = document.getElementById('#seconds')
+        let previousDiff = {}
+
+        const countdown = document.getElementById('countdown')
+        const launchDate = Date.parse(countdown.dataset.time) / 1000
+
+        function refreshCountdown() {
+            const difference = launchDate - Date.now() / 1000
+
+            if (difference <= 0) {
+                document.location.reload()
+                return
+            }
+            const diff = {
+                days: Math.floor(difference / DAYS),
+                hours: Math.floor(difference % DAYS / HOURS),
+                minutes: Math.floor(difference % HOURS / MINUTES),
+                seconds: Math.floor(difference % MINUTES)
+            }
+
+            updateDom(diff)
+
+            window.setTimeout(() => {
+                window.requestAnimationFrame(refreshCountdown)
+            }, 1000)
+        }
+
+        function updateDom(diff) {
+            Object.keys(diff).forEach(() => {
+                if (previousDiff[key] != diff[key]) {
+                    elements[key].innerText = diff[key]
+                }
+            })
+
+            previousDiff = diff
+
+            // if (previousDiff.days != diff.days) {
+            //     $days.innerText = diff.days
+            // }
+            
+            // if (previousDiff.hours != diff.hours) {
+            //     $hours.innerText = diff.hours
+            // }
+
+            // if (previousDiff.minutes != diff.minutes) {
+            //     $minutes.innerText = diff.minutes
+            // }
+
+            // if (previousDiff.seconds != diff.seconds) {
+            //     $seconds.innerText = diff.seconds
+            // }
+        }
+
+        refreshCountdown();
     </script>
 @endsection
